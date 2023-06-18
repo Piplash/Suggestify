6f390544e914ce1fbf9203f6b097542e36620038
Felipe Acosta <40440653+Piplash@users.noreply.github.com>
2023-06-18 17:22:41 -0400

    Actualizar dev.ts

--- src/environments/dev.ts
+++ src/environments/dev.ts
@@ -1,24 +1,7 @@
 
 export const environment = {
-
-    /*
-    redirect_uri: "http://localhost:4200/recomendaciones-musicales/descubre",
-    client_id: "0c31d1a6c73c4effac015f2c97d0a710",
-    client_secret: "a1f3fa5510ea47ccbd27cb093d8d8b41",
-    AUTH: "https://accounts.spotify.com/authorize"
-    */
-
-    /*PROD*/
-    redirect_uri: "https://piplash.github.io/Suggestify/recomendaciones-musicales/descubre",
-    client_id: "0c31d1a6c73c4effac015f2c97d0a710",
-    client_secret: "a1f3fa5510ea47ccbd27cb093d8d8b41",
-    AUTH: "https://accounts.spotify.com/authorize"
-    
-
-    /*
     redirect_uri: "YOUR_URI",
     client_id: "YOUR_CLIENT_ID",
     client_secret: "YOUR_CLIENT_SECRET",
     AUTH: "https://accounts.spotify.com/authorize"
-    */
 }
