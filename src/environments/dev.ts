22a36de309c44ae29ea415665fa4276575b503f4
Piplash <fibacosta@gmail.com>
2023-07-08 19:14:22 -0400

    cambio titulo

--- src/environments/dev.ts
+++ src/environments/dev.ts
@@ -1,17 +1,18 @@
 export const environment = {
 
-    /*redirect_uri: "http://localhost:4200/recomendaciones-musicales/descubre",
-    client_id: "0c31d1a6c73c4effac015f2c97d0a710",
-    client_secret: "a1f3fa5510ea47ccbd27cb093d8d8b41",
-    AUTH: "https://accounts.spotify.com/authorize"
-    */
-
-    /*PROD*/
-    redirect_uri: "https://piplash.github.io/Suggestify/recomendaciones-musicales/descubre",
+    redirect_uri: "http://localhost:4200/recomendaciones-musicales/descubre",
     client_id: "0c31d1a6c73c4effac015f2c97d0a710",
     client_secret: "a1f3fa5510ea47ccbd27cb093d8d8b41",
     AUTH: "https://accounts.spotify.com/authorize"
     
+
+    /*PROD*/
+    /*
+    redirect_uri: "https://piplash.github.io/Suggestify/recomendaciones-musicales/descubre",
+    client_id: "0c31d1a6c73c4effac015f2c97d0a710",
+    client_secret: "a1f3fa5510ea47ccbd27cb093d8d8b41",
+    AUTH: "https://accounts.spotify.com/authorize"
+    */
 
     /*
     redirect_uri: "YOUR_URI",
